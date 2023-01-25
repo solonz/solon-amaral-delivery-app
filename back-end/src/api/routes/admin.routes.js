ed1e22f4243b142bff7a175ef0fc4a3087fbf6f3
DanielGomesTB <danielgomesveiga@hotmail.com>
2023-01-25 16:55:43 -0300

    Arrumando erros de lint

--- back-end/src/api/routes/admin.routes.js
+++ back-end/src/api/routes/admin.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const registerController = require('../controllers/register.controller')
+const registerController = require('../controllers/register.controller');
+
 const routers = express.Router();
  
 // as rotas abaixo são complemento da rota /customer...
