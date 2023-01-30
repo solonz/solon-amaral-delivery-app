04a55b8bfa1ff9cc8565d8db278ee4896b066917
DanielGomesTB <danielgomesveiga@hotmail.com>
2023-01-30 19:37:37 -0300

    Arrumando lint

--- back-end/src/api/controllers/orders.controller.js
+++ back-end/src/api/controllers/orders.controller.js
@@ -3,7 +3,7 @@ const orderService = require('../services/orders.service');
 const getOrders = async (req, res) => {
   const { id } = req.user;
   const result = await orderService.getOrders(id);
-  console.log(result)
+  console.log(result);
   res.status(200).json(result);
 };
 
--- back-end/src/api/services/orders.service.js
+++ back-end/src/api/services/orders.service.js
@@ -2,9 +2,9 @@ const { Sale } = require('../../database/models');
 
 const getOrders = async (id) => {
     const response = await Sale.findAll({
-        where: { userId: id }
-    })
-    if(response.length > 0) return response;
+        where: { userId: id },
+    });
+    if (response.length > 0) return response;
     const error = { status: 404, message: 'No sales found' }; 
     throw error;
 };
